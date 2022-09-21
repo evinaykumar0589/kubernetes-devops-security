1913072a27cc9f7b2cf3731e13abf23b73e9bbaf
vinay <evinay.kumar0589@gmail.com>
2022-09-21 05:46:44 +0000

    testcase change to 50 from 49 and

--- src/test/java/com/devsecops/NumericApplicationTests.java
+++ src/test/java/com/devsecops/NumericApplicationTests.java
@@ -49,7 +49,6 @@ public class NumericApplicationTests {
     @Test
     public void welcomeMessage() throws Exception {
          this.mockMvc.perform(get("/")).andDo(print()).andExpect(status().isOk());
-	         .andExpect(cotent().string("kubernetes DevSecOps"))
     }
     
 
